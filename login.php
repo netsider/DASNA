40778a5a359d30ab5de45935f175ff5086528ea0
Russell Rounds <rdoubleoc@aol.com>
2015-05-04 06:32:39 -0400

    Commit: 3

--- login.php
+++ login.php
@@ -1,34 +1,51 @@
 <?php
 session_start();include_once('options.php');
 session_regenerate_id(true);$sid = session_id(); // To prevent session fixation
-$allgood = true;
 $TABLE = 'users';$database = 'dasna';
 if($_POST){ // If submit button has been pressed
-	echo '<pre>';
-	print_r($_POST);
-	echo '</pre><br/>';
-	foreach($_POST as $key => $value){
-	if (ctype_alnum($value)) {
-        echo '<font color="green">The field(<b>' . $key . '</b>) consists of all letters and/or digits.</font><br/>';
-    } else {
-		echo '<font color="red">The field(<b>' . $key . '</b>) does not consist of all letters and/or digits.</font><br/>';
-		$allgood = false; // if any values contain non-alpha characters, set false to prevent any possible injection.
-    }
-	}
-	if ($allgood){
+	// echo '<pre>';
+	// print_r($_POST);
+	// echo '</pre><br/>';
+	function allgood(){
+		$minlength = 1;
+		$maxlength = 100;
+		$fc = '<font color="red">';
+		$efc = '</font>';
+		foreach($_POST as $key => $value){
+			if(strlen($value) < $minlength){
+				$a = $fc . 'Length of <b>' . $key . '</b> is <b>' . strlen($value) . '</b>' . $efc;
+				echo $a;
+				return false;
+			}
+			if(strlen($value) > $maxlength){
+				echo $a;
+				return false;
+			}
+			if (ctype_alnum($value)) {
+				echo '<font color="green">The field(<b>' . $key . '</b>) is completely letters and/or digits.<br/>' . $efc;
+			} else {
+				echo $fc . 'The field(<b>' . $key . '</b>) is not completely letters and/or digits.<br/>' . $efc;
+				return false;
+			}
+	
+		}
+	return true;
+	};
+	if (allgood()){
 		$username = $_POST['in-user'];
 		$password = $_POST['in-pass'];
 		include_once('db.php');
 		mysqli_select_db($db, $database);
 		$result = mysqli_query($db, "SELECT * FROM users");
 		while($row = mysqli_fetch_array($result)){
-		if($row[0] == $username){
-		
+		if($row[1] === $username){
+			echo 'User <b>' . $row[1] . '</b> found!';
+			$user_exist = true;
 		}
 		}
-		echo '<pre>';
-		print_r($result);
-		echo '</pre><br/>';
+		// echo '<pre>';
+		// print_r($result);
+		// echo '</pre><br/>';
 	}
 }
 ?>
