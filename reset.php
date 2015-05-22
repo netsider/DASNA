af182266439b841b73511c232b5ffa68d94211d9
Russell Rounds <rdoubleoc@aol.com>
2015-05-22 05:36:46 -0400

    Commit: 11

--- reset.php
+++ reset.php
@@ -15,7 +15,7 @@ $text = "<tr><td>Password:</td><td><input type='password' name='in-pass' /></td>
 		mysqli_close($db);
 		return false;
 	};
-if($_POST){
+if($_POST['in-submit']){
 	$username = $_POST['in-user'];
 	$password = $_POST['in-pass'];
 	if(user_exist($username)){
@@ -24,7 +24,6 @@ if($_POST){
 		mysqli_select_db($db, database);
 		$query = "SELECT phash FROM users WHERE name = '$username'";
 		$result = mysqli_query($db, $query);
-			// $object = mysqli_fetch_object($result);
 			$array = mysqli_fetch_array($result);
 			echo '<font color="green">' . $array[0] . '</font><br/>';
 			echo strlen($array[0]);
@@ -33,16 +32,18 @@ if($_POST){
 				$pfp = true;
 				$output = '<font color="green"><b>Please type an alphanumeric password to continue</font></b>';
 			}else{
-				$output = '<font color="red"><b>Username already has password/value!</font></b>';
+				$output = '<font color="red"><b>Username not found, or already contains password.</font></b>';
 				$pfp = false;
 			}
-		// echo '<br/>';
-		// echo '<pre>';
-		// var_dump($result);
-		// print_r($result);
-		// echo '</pre>';
 	}else{
 		$output = '<b>User does not exist<b>!';
+	}
+}
+if(isset($_POST['in-user']) && isset($_POST['in-pass'])){
+	if(strlen($_POST['in-pass']) > 0){
+		$output = 'BOTH SET!';
+	}else{
+		$output = '<font color="red"><b>Password Blank!</b></font>';	
 	}
 }
 ?>
@@ -58,14 +59,18 @@ if($_POST){
 	<tr><td colspan="2"><center>Set Password</center></td></tr>
 	<form action='reset.php' method='POST'>
 	<tr><td>Username:</td><td><input type='text' name='in-user' <?php 
-	if($pfp){ echo "value='$username' disabled";};?> /></td></tr>
+	if($pfp){ echo "value='$username'";};?> /></td></tr>
 	<?php 
 	if($pfp){
 	echo $text;};?>
-	<tr><td colspan='2'><input type='submit' name='in-submit' value='Login' /><?php if(isset($output)){
+	<tr><td colspan='2'><input type='submit' name='in-submit' <?php if($pfp){ echo "value='Change Password'"; }else{ echo 'Submit'; }; ?> /><?php if(isset($output)){
 	echo $output;}; ?></td></tr>
 	</form>
 	</table>
 	</center>
 </body>
 </html>
+<?php
+	echo '<pre>';
+	print_r($_POST);
+	echo '</pre>';
