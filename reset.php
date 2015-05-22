83ded8686d4b83808a2118a84670fe02213b1ebe
Russell Rounds <rdoubleoc@aol.com>
2015-05-22 04:46:19 -0400

    Commit: 8

--- reset.php
+++ reset.php
@@ -1,13 +1,14 @@
 <?php
-session_start();include_once('options.php');
+error_reporting(E_ALL ^ E_NOTICE);session_start();include_once('options.php');
 session_regenerate_id(true);$sid = session_id(); // To prevent session fixation
-	function user_exist($user){
-		include_once('db.php');
-		mysqli_select_db($db, $database);
+const database = 'dasna';
+$text = "<tr><td>Password:</td><td><input type='password' name='in-pass' /></td></tr>";
+	function user_exist($u){
+		include('db.php');
+		mysqli_select_db($db, database);
 		$result = mysqli_query($db, "SELECT name FROM users");
 		while($row = mysqli_fetch_array($result)){
-			if($row[0] === $username){
-				echo 'User <b>' . $username . '</b> found!';
+			if($row[0] === $u){
 				return true;
 			}
 		}
@@ -18,7 +19,29 @@ if($_POST){
 	$username = $_POST['in-user'];
 	$password = $_POST['in-pass'];
 	if(user_exist($username)){
-		echo 'User exists!';
+		// echo 'User exists!';
+		include('db.php');
+		mysqli_select_db($db, database);
+		$query = "SELECT phash FROM users WHERE name = '$username'";
+		$result = mysqli_query($db, $query);
+			// $object = mysqli_fetch_object($result);
+			$array = mysqli_fetch_array($result);
+			echo '<font color="green">' . $array[0] . '</font><br/>';
+			echo strlen($array[0]);
+			if($array[0] === NULL){
+				// echo 'NULL!<br/>';
+				$pfp = true;
+			}else{
+				$output = '<font color="red"><b>Username already has password/value!</font></b>';
+				$pfp = false;
+			}
+		echo '<br/>';
+		echo '<pre>';
+		var_dump($result);
+		print_r($result);
+		echo '</pre>';
+	}else{
+		$output = '<b>User does not exist<b>!';
 	}
 }
 ?>
@@ -30,12 +53,15 @@ if($_POST){
 </head>
 <body>
 	<center>
-	<table width='20%' border='1'>
-	<tr><td colspan="2"><?php echo $sid; ?></td></tr>
+	<table width='10%' border='1'>
+	<tr><td colspan="2"></td></tr>
 	<form action='reset.php' method='POST'>
 	<tr><td>Username:</td><td><input type='text' name='in-user' /></td></tr>
-	<tr><td>Password:</td><td><input type='password' name='in-pass' /></td></tr>
-	<tr><td colspan='2'><input type='submit' name='in-submit' value='Login' /></td></tr>
+	<?php 
+	if($pfp){
+	echo $text;};?>
+	<tr><td colspan='2'><input type='submit' name='in-submit' value='Login' /><?php if(isset($output)){
+	echo $output;}; ?></td></tr>
 	</form>
 	</table>
 	</center>
