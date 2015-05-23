61ca9aa1ecdd20d97a1d1fd2c933d529cb231e08
Russell Rounds <rdoubleoc@aol.com>
2015-05-23 04:01:15 -0400

    Commit: 12

--- reset.php
+++ reset.php
@@ -25,27 +25,43 @@ if($_POST['in-submit']){
 		$query = "SELECT phash FROM users WHERE name = '$username'";
 		$result = mysqli_query($db, $query);
 			$array = mysqli_fetch_array($result);
-			echo '<font color="green">' . $array[0] . '</font><br/>';
-			echo strlen($array[0]);
+			$plength = strlen($array[0]);
+			$phash = $array[0];
+			echo '<font color="green">' . $phash . '</font><br/>';
+			echo '<font color="blue">' . $plength . '</font><br/>';
 			if($array[0] === NULL){
 				// echo 'NULL!<br/>';
-				$pfp = true;
-				$output = '<font color="green"><b>Please type an alphanumeric password to continue</font></b>';
+				$null = true;
+				$output = '<font color="green"><b>Type an alphanumeric password to be your password.</font></b>';
 			}else{
+				$null = false;
 				$output = '<font color="red"><b>Username not found, or already contains password.</font></b>';
-				$pfp = false;
 			}
 	}else{
 		$output = '<b>User does not exist<b>!';
 	}
-}
-if(isset($_POST['in-user']) && isset($_POST['in-pass'])){
-	if(strlen($_POST['in-pass']) > 0){
-		$output = 'BOTH SET!';
-	}else{
-		$output = '<font color="red"><b>Password Blank!</b></font>';	
+	if($null){
+		if(isset($_POST['in-user']) && isset($_POST['in-pass'])){
+			$plength = strlen($_POST['in-pass']);
+			$ulength = strlen($_POST['in-user']);
+			if($plength > 0){
+				// if passwork not blank
+				$set = true;
+				$output = 'BOTH SET!';
+			}else{
+				$output = '';	
+			}
+		}
+	}
+	if($_POST['in-submit'] === "Confirm Password"){
+	$set = true;
+		echo 'Password Confirmed!';
+		echo '<pre>';
+	print_r($_POST);
+	echo '</pre><br/>';
 	}
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -59,18 +75,35 @@ if(isset($_POST['in-user']) && isset($_POST['in-pass'])){
 	<tr><td colspan="2"><center>Set Password</center></td></tr>
 	<form action='reset.php' method='POST'>
 	<tr><td>Username:</td><td><input type='text' name='in-user' <?php 
-	if($pfp){ echo "value='$username'";};?> /></td></tr>
+	if(isset($username)){ echo "value='$username'";}; if(isset($username)){ echo 'disabled';};?> /></td></tr>
 	<?php 
-	if($pfp){
-	echo $text;};?>
-	<tr><td colspan='2'><input type='submit' name='in-submit' <?php if($pfp){ echo "value='Change Password'"; }else{ echo 'Submit'; }; ?> /><?php if(isset($output)){
+	if($null){
+	echo "<tr><td><b>New Password</b>:</td><td><input type='password' name='in-pass'";
+	if(isset($password)){
+		echo "value='$password'";
+		echo ' disabled';
+	}
+	echo "/></td></tr>";
+	};
+	if(isset($username)){
+		echo "<input type='hidden' name='in-user' value='$username'>";
+	}
+	if(isset($password)){
+		echo "<input type='hidden' name='in-pass' value='$password'>";
+	}
+	if($set){
+		echo "<tr><td><b>Re-type Password</b>:</td><td><input type='text' name='in-pass-new'";
+		if(isset($_POST['in-pass-new'])){
+			$newpass = $_POST['in-pass-new'];
+			echo "value='$newpass'";
+			echo ' disabled';
+		}
+	}
+	echo '/></td></tr>';
+	?>
+	<tr><td colspan='2'><input type='submit' name='in-submit' <?php if($null){ echo "value='Confirm Password'"; }else{ echo'Submit'; } ?> /><?php if(isset($output)){
 	echo $output;}; ?></td></tr>
-	</form>
-	</table>
-	</center>
+	</center><br/>
+	<?php echo '</form></table>'; ?>
 </body>
 </html>
-<?php
-	echo '<pre>';
-	print_r($_POST);
-	echo '</pre>';
