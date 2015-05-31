2b8df0a170c0d687e4637ad32bb861d8c867d9df
Russell Rounds <rdoubleoc@aol.com>
2015-05-30 23:12:39 -0400

    Revert "Commit: 21"
    This reverts commit da5a50eafe3f114941e576255cbfa80f0d2a0e5e.

--- reset.php
+++ reset.php
@@ -34,49 +34,37 @@ $text = "<tr><td>Password:</td><td><input type='password' name='in-pass' /></td>
 		return false;
 	};
 	function allgood($array){ // returns false if not alphanumeric
-		$alpha = true;
 		$fc = '<font color="red">';
 		$efc = '</font><br/>';
 		foreach($array as $key => $value){
 			if (ctype_alnum($value)) {
 				echo '<font color="green">The field (<b>' . $key . '</b>) is completely alphanumeric.' . $efc;
+				return true;
 			} else {
-				$alpha = false;
-				if(empty($value)){
-					echo $fc . '<b>Field Empty!</b>' . $efc;
-				}else{
-					echo $fc . 'The field (<b>' . $key . '</b>) is not completely alphanumeric.' . $efc;
-				}
+				echo $fc . 'The field (<b>' . $key . '</b>) is not completely alphanumeric.' . $efc;
+				return false;
 			}
 		}
-		if($alpha){
-			return true;
-		}else{
-			echo '<pre>';
-			print_r($_POST);
-			echo '</pre><br/>';
-			return false;
-		}
 	};
-if($_POST['in-submit']){
-	if(allgood($_POST)){ 
+if($_POST['in-submit']){ // executed everytime submit button is pressed
+	if(allgood($_POST)){ // Filter input to prevent injection attacks
 		$username = $_POST['in-user'];
 		if(isset($_POST['in-pass'])){
 			$password = $_POST['in-pass'];
 		}
-		if(user_exist($username)){
-			if(phash_null($username) === true){
-				$output = '<font color="green"><b>Type an alphanumeric password to be your password.</font></b>';
-				$null = true;
-			}else{
-				$output = '<font color="red"><b>Username not found, or not NULL!</font></b>';
-				$null = false;
-			}
-		}else{
-			$output = '<b>User does not exist<b>!';
-		}
 	}
-	if($null){
+	if(user_exist($username)){
+		if(phash_null($username) === true){
+			$output = '<font color="green"><b>Type an alphanumeric password to be your password.</font></b>';
+			$null = true;
+		}else{
+			$output = '<font color="red"><b>Username not found, or already contains password.</font></b>';
+			$null = false;
+		}
+	}else{
+		$output = '<b>User does not exist<b>!';
+	}
+	if($null){ // executed if password is NULL
 		if(isset($_POST['in-user']) && isset($_POST['in-pass'])){
 			$plength = strlen($_POST['in-pass']);
 			$ulength = strlen($_POST['in-user']);
@@ -88,13 +76,13 @@ if($_POST['in-submit']){
 				$output = '';	
 			}
 		}
-		if($_POST['in-submit'] === "Confirm"){ // executed when all critera met
-			// $set = true;
+	}
+	if($_POST['in-submit'] === "Confirm Password"){ // executed when all critera met
+	$set = true;
+		$output = 'Password Confirmed!';
 		echo '<pre>';
-		print_r($_POST);
-		echo '</pre><br/>';
-		$output = 'Password Set!';
-		}
+	print_r($_POST);
+	echo '</pre><br/>';
 	}
 }
 ?>
@@ -134,11 +122,13 @@ if($_POST['in-submit']){
 		echo "<tr><td><b>Security Question #1:</b>:</td><td><b>Security Question #2:</b>:</td></tr>";
 		echo "<tr><td>";
 		echo '<select name="in-ques-1">';
+		echo '<option value="birthplace">Birthplace</option><option value="firstlove">First Girlfriend/Boyfriend</option>';
 		echo '<option value="favfood">Favorite Food</option><option value="favbook">Favorite Book</option>';
 		echo "</select>";
 		echo "</td><td>";
 		echo '<select name="in-ques-2">';
 		echo '<option value="birthplace">Birthplace</option><option value="firstlove">First Girlfriend/Boyfriend</option>';
+		echo '<option value="favfood">Favorite Food</option><option value="favbook">Favorite Book</option>';
 		echo "</select>";
 		echo "</td></tr>";
 		echo "<tr><td><input type='text' name='in-ans-1'/></td><td><input type='text' name='in-ans-2'/></td></tr>";
@@ -150,7 +140,7 @@ if($_POST['in-submit']){
 		echo "<input type='hidden' name='in-pass' value='$password'>";
 	}
 	?>
-	<tr><td colspan='2'><input type='submit' name='in-submit' <?php if($null){ echo "value='Confirm'"; }else{ echo'Submit'; } ?> /><?php if(isset($output)){
+	<tr><td colspan='2'><input type='submit' name='in-submit' <?php if($null){ echo "value='Confirm Password'"; }else{ echo'Submit'; } ?> /><?php if(isset($output)){
 	echo $output;}; ?></td></tr>
 	</center><br/></form></table>
 </body>
