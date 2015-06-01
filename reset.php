bbec05f7b4066d4dfcbfa96a7eea097229ac41f6
Russell Rounds <rdoubleoc@aol.com>
2015-06-01 00:29:57 -0400

    Commit: 23

--- reset.php
+++ reset.php
@@ -141,7 +141,7 @@ if($_POST['in-submit']){ // executed everytime submit button is pressed
 	}
 	?>
 	<tr><td colspan='2'><input type='submit' name='in-submit' <?php if($null){ echo "value='Confirm Password'"; }else{ echo'Submit'; } ?> /><?php if(isset($output)){
-		echo $output;}; 
+		echo $output;};
 	?></td></tr></center><br/></form></table>
 </body>
 </html>
