01afc06e908a89d233e51460a8a3c85180f37266
Russell Rounds <rdoubleoc@aol.com>
2015-06-03 00:07:11 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	php-mailer.php

--- php-mailer.php
+++ php-mailer.php
@@ -1,28 +1,50 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);
-include('class.phpmailer.php');
+error_reporting(E_ALL);ini_set('display_errors',1);
+date_default_timezone_set("America/New_york");
+require('class.phpmailer.php');
+require('class.smtp.php');
+clearstatcache();
+if(ini_get('allow_url_fopen')) {
+   echo 'Fopen on!<br/>';
+}else{
+	echo 'Fopen off!<br/>';
+}
+echo (extension_loaded('openssl')?'OpenSSL extension loaded!':'SSL not loaded')."<br/>";
+$support = 'dasnashire@yahoo.com';
+// echo shell_exec("sudo setsebool httpd_can_network_connect=1");
 $mail = new PHPMailer();
+// $mail->CharSet = 'UTF-8';
 $mail->IsSMTP();
+$mail->ContentType = 'text/plain';  
+$mail->IsHTML(false);
+$mail->SMTPDebug = 4;
 $mail->SMTPAuth = true;
-$mail->SMTPSecure = 'ssl'; 
+$mail->SMTPSecure = 'tls'; 
 $mail->Host = 'smtp.mail.yahoo.com';
-$mail->Port = 465;
-$mail->Username = 'admin@dasna.net';
-$mail->Password = 'SDxv5AR428XHRJ63z%6C5N6lW9^1u4X8'; 
-$mail->From = 'mailserver@dasna.net';
-$mail->FromName = 'mailserver@dasna.net';
-// $mail->AddAddress('rdoubleoc@aol.com');
-$mail->AddAddress('rdoubleoc@gmail.com');
-$mail->AddAddress('14434972008@vtext.com');
-$mail->Subject = "Test Message";
+$mail->Port = 587;
+$mail->Username = $support;
+$mail->Password = ''; 
+$mail->From = $support;
+$mail->FromName = $support;
+$mail->Subject = 'Test Message';
 $mail->Body = 'Hello World';
+$mail->AddAddress('4434972008@vtext.com');
+$mail->AddCC('rdoubleoc@aol.com');
+$mail->AddCC('rdoubleoc@gmail.com');
+echo 'Version 1.28<br/>'; 
 echo 'Attempting to send text message... <br/>';
-echo 'Version 1.15<br/>'; 
+// echo $mail->ErrorInfo;
 if(!$mail->Send()){
     echo 'Message was not sent';
     echo 'Mailer error: ' . $mail->ErrorInfo;
 }
 else{
-    echo 'Message has been sent.';
+    echo 'Message has been sent.<br/>';
 }
+echo $mail->Host . '<br/>';
+echo $mail->Username . '<br/>';
+echo $mail->Port . '<br/>';
+echo '<pre>';
+var_export($_REQUEST);
+echo '</pre>';
 ?>
