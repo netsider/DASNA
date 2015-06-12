36f3bf399fbdaf8119f4ec18c95506a4480b6682
Russell Rounds <rdoubleoc@aol.com>
2015-06-12 01:45:30 -0400

    Commit: 64
    Fixed editor.php so that it sends an ajax request on both onkeydown and
    onkeyup, because the last character typed wasn't being saved.

--- editor.php
+++ editor.php
@@ -8,7 +8,7 @@ if($A = read_content('A')){
 	echo '<tr><td colspan="2"><center>Edit HTML</center></td></tr>';
 	echo '<form action="editor.php" method="POST">';
 	echo '<tr><td colspan="2">';
-	echo '<textarea rows="30" cols="80" spellcheck="true" id="editor" onkeydown="myFunction()">' . htmlspecialchars($A) . '</textarea>';
+	echo '<textarea rows="30" cols="80" spellcheck="true" id="editor" onkeydown="myFunction()" onkeyup="myFunction()">>' . htmlspecialchars($A) . '</textarea>';
 	echo '</td></tr>';
 	echo '<tr><td colspan="2"><input type="submit" name="in-submit" value="Login" /></td></tr>';
 	echo '</form></table></center>';
